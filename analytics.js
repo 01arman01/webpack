3552a9c7b7f69b798cf701ad6f26480b02d1a645
ArmanPapyan <49716257+ArmanPapyan@users.noreply.github.com>
2020-01-27 16:06:56 +0400

    create

---
+++ analytics.js
@@ -0,0 +1,23 @@
+function createAnalytics (){
+    let counter = 0;
+    let isdestroyed = false
+
+    const listeneer = ()=> counter++
+    document.addEventListener('click' ,listeneer)
+    
+    return{
+        destroy(){  
+            document.removeEventListener('click' ,listeneer)
+            isdestroyed = true
+        },
+        getClicks(){
+            if(isdestroyed){
+               return 'analytic is destroyed'
+            }else{
+            return counter
+            }
+        }
+    }
+}
+
+window.analytics = createAnalytics()
---
+++ index.js
@@ -0,0 +1,3 @@
+const post = new Post('Webpack post title')
+
+console.log('post to string',post.toString())
